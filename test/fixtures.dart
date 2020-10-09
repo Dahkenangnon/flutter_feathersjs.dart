729a849e83a7ad9fc22488d2bf10f9fa19522bec
Justin Dah-kenangnon <dah.kenangnon@gmail.com>
2020-10-09 20:20:55 +0200

    remove some txt from fixture.dart

--- test/fixtures.dart
+++ test/fixtures.dart
@@ -1,11 +1,11 @@
 //User credentials used for auth purpose
 Map<String, String> user = {
   "strategy": "local",
-  "email": "justin@dah.com",
-  "password": "justin"
+  "email": "email@user.com",
+  "password": "password"
 };
 
 //your use api
-const BASE_URL = "https://api.epatriote.com";
+const BASE_URL = "https://api.domain.com";
 
 // adhara_socket_io
