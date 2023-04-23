b049a39cde6130237db55dcc479aa5b4958f0a27
Dahkenangnon <dah.kenangnon@gmail.com>
2023-04-23 19:21:59 +0000

    Resolve import

--- lib/flutter_feathersjs.dart
+++ lib/flutter_feathersjs.dart
@@ -16,7 +16,7 @@
 ///
 library flutter_feathersjs;
 
-export './src/config/constants.dart' show FeatherJsErrorType;
-export './src/config/helper.dart' show FeatherJsError;
-export './src/config/storage.dart' show JsonStorage;
+export './src/config/constants.dart';
+export './src/config/helper.dart';
+export './src/config/storage.dart';
 export './src/featherjs.dart';
